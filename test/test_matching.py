9933824a2e07f2e96d450cc7be53d71180d26690
digiwhale <digiwhale.humphrey@gmail.com>
2022-04-03 21:33:18 -0400

    1

--- test/test_matching.py
+++ test/test_matching.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     test_match()
     
     # Test the match_from_geojson function
-    dirname = "/home/pi/noiseplanet/test/data"
+    dirname = "/home/pi/noiseplanet/test/data/track/"
     out_dirname = '.'
     test_match_from_geojsons(dirname, out_dirname=out_dirname, method="nearest", log=True)
     test_match_from_geojsons(dirname, out_dirname=out_dirname, method="hmm", log=True)
